be8efe486ddee413b197e3ab14fd374d3da8f7c0
Aaron Straup Cope <thisisaaronland@users.noreply.github.com>
2025-11-04 18:27:08 -0800

    snapshot: update to do parsed flags dance (#53)
    Co-authored-by: thisisaaronland <thisisaaronland@localhost>

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"context"
-	"log/slog"
-	"os"
+	"log"
 
 	"github.com/whosonfirst/go-whosonfirst-spelunker-httpd/app/server"
 )
@@ -11,12 +10,9 @@ import (
 func main() {
 
 	ctx := context.Background()
-	logger := slog.Default()
-
-	err := server.Run(ctx, logger)
+	err := server.Run(ctx)
 
 	if err != nil {
-		slog.Error("Failed to run server", "error", err)
-		os.Exit(1)
+		log.Fatalf("Failed to run server, %v", err)
 	}
 }
